887b48b6f14931bf46a304132561e2026e515779
nikachkharti <nchkhartishvili@novasystems.ge>
2023-03-11 17:18:59 +0400

    Models added

--- FastFoodApplication/FastFood.Models/Employee.cs
+++ FastFoodApplication/FastFood.Models/Employee.cs
@@ -2,5 +2,9 @@
 {
     public class Employee
     {
+        public int Id { get; set; }
+        public string Pin { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
     }
 }
---
+++ FastFoodApplication/FastFood.Models/Order.cs
@@ -0,0 +1,12 @@
+﻿namespace FastFood.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public int EmployeeId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public Product Product { get; set; }
+        public Employee Employee { get; set; }
+    }
+}
